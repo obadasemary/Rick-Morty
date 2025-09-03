6e884dd9023746c98d4cda78f938f1e9c586e1de
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-09-03 18:07:26 +0300

    chore: remove Mac platform

--- RickMortyNetworkLayer/Package.swift
+++ RickMortyNetworkLayer/Package.swift
@@ -6,8 +6,7 @@ import PackageDescription
 let package = Package(
     name: "RickMortyNetworkLayer",
     platforms: [
-        .iOS(.v17),
-        .macOS(.v14),
+        .iOS(.v17)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
