709567656662017e76457c82e4023e2469993c96
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-09-24 18:50:02 +0300

    chore: fix force unwrapped in `AppComposition`

--- RickMorty/App/AppComposition.swift
+++ RickMorty/App/AppComposition.swift
@@ -16,26 +16,34 @@ import RickMortyNetworkLayer
 final class AppComposition {
     
     var container = DIContainer()
-    let feedUseCase: FeedUseCaseProtocol
+    let feedUseCase: FeedUseCaseProtocol?
+    let isConfigured: Bool
     
     init() {
         let container = DIContainer()
+        var configured = true
+        var useCase: FeedUseCaseProtocol?
         
-        container.register(NetworkService.self) {
-            URLSessionNetworkService(session: .shared)
+        // Register NetworkService
+        container.register(NetworkService.self, URLSessionNetworkService(session: .shared))
+        
+        // Register FeedRepository with safe resolution
+        do {
+            let networkService = try container.requireResolve(NetworkService.self)
+            container.register(FeedRepositoryProtocol.self, FeedRepository(networkService: networkService))
+            
+            // Register FeedUseCase
+            container.register(FeedUseCaseProtocol.self, FeedUseCase(container: container))
+            
+            // Resolve FeedUseCase
+            useCase = try container.requireResolve(FeedUseCaseProtocol.self)
+        } catch {
+            print("Failed to configure dependencies: \(error)")
+            configured = false
         }
         
-        container.register(FeedRepositoryProtocol.self) {
-            FeedRepository(
-                networkService: container.resolve(NetworkService.self)!
-            )
-        }
-        
-        container.register(FeedUseCaseProtocol.self) {
-            FeedUseCase(container: container)
-        }
-        
-        feedUseCase = container.resolve(FeedUseCaseProtocol.self)!
+        self.feedUseCase = useCase
+        self.isConfigured = configured
         self.container = container
     }
 }
