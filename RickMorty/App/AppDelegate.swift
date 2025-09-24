80dec4fd9fce4b53f0575c91adf8da9d1edfe7b6
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-09-24 18:50:19 +0300

    chore: fix force unwrapped in `AppDelegate`

--- RickMorty/App/AppDelegate.swift
+++ RickMorty/App/AppDelegate.swift
@@ -13,13 +13,13 @@ import CharacterDetailsView
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     
-    var composition: AppComposition!
+    var composition: AppComposition?
     
     // Builders
-    var tabBarBuilder: TabBarBuilder!
-    var feedBuilder: FeedBuilder!
-    var feedListBuilder: FeedListBuilder! // Hybrid UIKit/SwiftUI Architecture
-    var characterDetailsBuilder: CharacterDetailsBuilder!
+    var tabBarBuilder: TabBarBuilder?
+    var feedBuilder: FeedBuilder?
+    var feedListBuilder: FeedListBuilder? // Hybrid UIKit/SwiftUI Architecture
+    var characterDetailsBuilder: CharacterDetailsBuilder?
     
     func application(
         _ application: UIApplication,
@@ -27,13 +27,22 @@ class AppDelegate: NSObject, UIApplicationDelegate {
     ) -> Bool {
         
         // Setup DI container
-        composition = AppComposition()
+        let appComposition = AppComposition()
         
-        // Register builders
-        tabBarBuilder = TabBarBuilder(container: composition.container)
-        feedBuilder = FeedBuilder(container: composition.container)
-        feedListBuilder = FeedListBuilder(container: composition.container)
-        characterDetailsBuilder = CharacterDetailsBuilder(container: composition.container)
+        // Check if composition was successful
+        guard appComposition.isConfigured else {
+            print("Failed to configure app dependencies")
+            // You could show an error screen or handle this gracefully
+            return false
+        }
+        
+        composition = appComposition
+        
+        // Register builders safely
+        tabBarBuilder = TabBarBuilder(container: appComposition.container)
+        feedBuilder = FeedBuilder(container: appComposition.container)
+        feedListBuilder = FeedListBuilder(container: appComposition.container)
+        characterDetailsBuilder = CharacterDetailsBuilder(container: appComposition.container)
         
         return true
     }
